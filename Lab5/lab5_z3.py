4c250526e125b9b2006398da1ae3c13777f7c0b6
Folonder <74664782+Folonder@users.noreply.github.com>
2023-06-07 14:20:06 +0300

    finish 5 lab

--- Lab5/lab5_z3.py
+++ Lab5/lab5_z3.py
@@ -3,6 +3,8 @@ from collections import defaultdict
 from prettytable import PrettyTable
 import warnings
 warnings.filterwarnings("ignore")
+import matplotlib
+matplotlib.use('Qt5Agg')
 
 
 class Distribution:
@@ -24,14 +26,22 @@ class Distribution:
         # self.crate_relative_frequency_polygon()
         # self.create_bar_plot()
 
-    def read_interval_row(self):
+    def read_interval_row(self, console=True):
         interval_row = dict()
-        with open("z3_var23.txt") as f:
-            string = f.readline()
-            vars = [tuple(map(float, i.split(";"))) for i in string.split(" ")]
-            values = list(map(int, f.readline().split()))
-            for i in range(len(vars)):
-                interval_row[vars[i]] = values[i]
+        if console:
+            vars = list(map(float, input("Введите границы интервалов через точку с запятой: ").split(';')))
+            values = list(map(float, input("Введите значения интервалов через точку с запятой: ").split(';')))
+            if len(vars) != len(values) + 1:
+                print("Количество интервалов не соответствует количествву значений")
+            for i in range(len(values)):
+                interval_row[(vars[i], vars[i + 1])] = values[i]
+        else:
+            with open("z3_var23.txt") as f:
+                string = f.readline()
+                vars = [tuple(map(float, i.split(";"))) for i in string.split(" ")]
+                values = list(map(int, f.readline().split()))
+                for i in range(len(vars)):
+                    interval_row[vars[i]] = values[i]
         return interval_row
 
     def get_stat_frequency(self):
@@ -64,53 +74,113 @@ class Distribution:
         return (self.variance * self.n / (self.n - 1)) ** 0.5
 
     def create_func_graph(self):
-        x = [key[1] for key in self.stat_frequency.keys()]
-        x = [min(x[0] - abs(x[0] * 1), 0)] + x + [x[-1] + 1]
-        y = list(self.distribution_func.keys()) + [1]
-        plt.step(x, y, where='post')
-        plt.scatter(x[1:-1], y[1:-1], marker='o', facecolors='white', edgecolors='blue', zorder=10)
-        plt.xticks(x[:-1])
-        plt.yticks([i * 0.1 for i in range(11)])
-        plt.xlabel('x')
-        plt.ylabel('F*(x)')
-        plt.show()
+        fig, ax = plt.subplots(1, 1)
+        a = min(list(self.stat_frequency.keys())[0][0], 0) - 2
+        b = list(self.stat_frequency.keys())[0]
+        ax.plot((min(list(self.stat_frequency.keys())[0][0], 0) - 2, list(self.stat_frequency.keys())[0][1]), (0, 0),
+                color='b')
+        for part in list(self.distribution_func.items())[1:-1]:
+            x1, y1 = (part[1][0], part[0])
+            x2, y2 = part[1][1], part[0]
+            ax.plot((x1, x2), (y1, y2), color='b')
+        ax.plot((list(self.stat_frequency.keys())[-1][1], list(self.stat_frequency.keys())[-1][1] + 2), (1, 1),
+                color='b')
+        x_axes = [i[0] for i in self.distribution_func.values()]
+        ax.plot(x_axes[1:], [key for key in self.distribution_func.keys()][1:], 'bo')
+        ax.plot(x_axes[1:], [key for key in self.distribution_func.keys()][1:], 'w.')
+        # lx = list(self.distribution_func.items())[0][1][0]
+        # rx = list(self.distribution_func.items())[-1][1][1]
+        # #
+        # ax.set_xbound(lx, rx)
+        ax.set_ybound(0, 1.1)
+        ax.set_xlabel('x')
+        ax.set_ylabel('F*(x)')
+        ax.grid(True)
+        plt.subplot(1, 1, 1)
+        # x = [key[1] for key in self.stat_frequency.keys()]
+        # x = [min(x[0] - abs(x[0] * 1), 0)] + x + [x[-1] + 1]
+        # y = list(self.distribution_func.keys()) + [1]
+        # plt.step(x, y, where='post')
+        # plt.scatter(x[1:-1], y[1:-1], marker='o', facecolors='white', edgecolors='blue', zorder=10)
+        # plt.xticks(x[:-1])
+        # plt.yticks([i * 0.1 for i in range(11)])
+        # plt.xlabel('x')
+        # plt.ylabel('F*(x)')
+        # plt.show()
 
     def create_frequency_polygon(self):
+        fig, ax = plt.subplots(1, 1)
         x = [round((key[0] + key[1]) / 2, 2) for key in self.stat_frequency.keys()]
         y = list(self.stat_frequency.values())
-        plt.plot(x, y)
-        plt.xlim([0, max(x) * 2])
-        plt.ylim([0, max(y) * 2])
-        plt.xticks([0] + x)
-        plt.yticks(range(round(min(min(y), 0), 2), round(max(y) * 2), 2))
-        plt.scatter(x, y)
-        plt.xlabel('x')
-        plt.ylabel('ni')
-        plt.show()
+        ax.plot(x, y)
+        ax.set_xbound(0, round(max(x) * 1.1))
+        ax.set_ybound(0, round(max(y) * 1.1))
+        # ax.xticks([0] + x)
+        # ax.yticks([0] + y)
+        ax.scatter(x, y)
+        ax.set_xlabel('x')
+        ax.set_ylabel('ni')
+        ax.grid(True)
+        plt.subplot(1, 1, 1)
+        # x = [round((key[0] + key[1]) / 2, 2) for key in self.stat_frequency.keys()]
+        # y = list(self.stat_frequency.values())
+        # plt.plot(x, y)
+        # plt.xlim([0, max(x) * 2])
+        # plt.ylim([0, max(y) * 2])
+        # plt.xticks([0] + x)
+        # plt.yticks(range(round(min(min(y), 0), 2), round(max(y) * 2), 2))
+        # plt.scatter(x, y)
+        # plt.xlabel('x')
+        # plt.ylabel('ni')
+        # plt.show()
 
     def create_relative_frequency_polygon(self):
-        x = [round((key[0] + key[1]), 2) / 2 for key in self.stat_relative_frequency.keys()]
+        fig, ax = plt.subplots(1, 1)
+        x = [round((key[0] + key[1]) / 2, 2) for key in self.stat_relative_frequency.keys()]
         y = list(self.stat_relative_frequency.values())
-        plt.plot(x, y)
-        plt.xlim([0, max(x) * 2])
-        plt.ylim([0, 1])
-        plt.xticks(x)
-        plt.yticks([i * 0.1 for i in range(11)])
-        plt.scatter(x, y)
-        plt.xlabel('x')
-        plt.ylabel('ni/n')
-        plt.show()
+        ax.plot(x, y)
+        ax.set_xbound(0, round(max(x) * 1.1))
+        ax.set_ybound(0, 1)
+        # ax.xticks([0] + x)
+        # ax.yticks([i * 0.1 for i in range(11)])
+        ax.scatter(x, y)
+        ax.set_xlabel('x')
+        ax.set_ylabel('ni/n')
+        ax.grid(True)
+        plt.subplot(1, 1, 1)
+        # x = [round((key[0] + key[1]), 2) / 2 for key in self.stat_relative_frequency.keys()]
+        # y = list(self.stat_relative_frequency.values())
+        # plt.plot(x, y)
+        # plt.xlim([0, max(x) * 2])
+        # plt.ylim([0, 1])
+        # plt.xticks(x)
+        # plt.yticks([i * 0.1 for i in range(11)])
+        # plt.scatter(x, y)
+        # plt.xlabel('x')
+        # plt.ylabel('ni/n')
+        # plt.show()
 
     def create_bar_plot(self):
+        fig, ax = plt.subplots(1, 1)
         first = list(self.stat_frequency.keys())[0]
         x = [key[0] for key in self.stat_frequency.keys()] + [max(self.stat_frequency.keys(), key=lambda x: x[1])[1]]
         y = [value / (key[1] - key[0]) for key, value in self.stat_frequency.items()]
-        plt.bar(x[:-1], y, width=(first[1] - first[0]), align='edge')
-        plt.xticks(x)
-        plt.yticks(y)
-        plt.xlabel('x')
-        plt.ylabel('ni/h')
-        plt.show()
+        ax.bar(x[:-1], y, width=(first[1] - first[0]), align='edge')
+        ax.set_xticks(x)
+        ax.set_yticks(y)
+        ax.set_xlabel('x')
+        ax.set_ylabel('ni/h')
+        ax.grid(True)
+        plt.subplot(1, 1, 1)
+        # first = list(self.stat_frequency.keys())[0]
+        # x = [key[0] for key in self.stat_frequency.keys()] + [max(self.stat_frequency.keys(), key=lambda x: x[1])[1]]
+        # y = [value / (key[1] - key[0]) for key, value in self.stat_frequency.items()]
+        # plt.bar(x[:-1], y, width=(first[1] - first[0]), align='edge')
+        # plt.xticks(x)
+        # plt.yticks(y)
+        # plt.xlabel('x')
+        # plt.ylabel('ni/h')
+        # plt.show()
 
 
 def show_help():
@@ -162,6 +232,7 @@ def show_all(dist: Distribution):
     show_func(dist)
     print()
     show_characteristics(dist)
+    plt.show()
 
 
 if __name__ == "__main__":
